4ec276ee8f6e03784219024a88175e638d94f4ae
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-26 04:30:34 +0600

    variable-name file added

---
+++ variable-name.js
@@ -0,0 +1,14 @@
+var name = 'Hero Alan';
+
+// reserved keyword not allowed
+// var false = 15;
+// var delete = 'delete';
+
+// name should be one word 
+// var my name = 'mofu fdfola'
+
+// quote is not allowed
+// var "fadhfsa" = 'shakil';
+
+// var-office-address = 'dhaka';
+var OfficeAddress = 'dhaka';
