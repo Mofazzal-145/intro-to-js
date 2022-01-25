2d9bf31f01fdcb34283dc2e84b66b648e5a7f956
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-26 05:36:35 +0600

    mathematical-operation file added

---
+++ mathematical-operaton.js
@@ -0,0 +1,20 @@
+var onionPrice = 42;
+var eggPrice = 43;
+var eggQuantity = 7;
+console.log(onionPrice);
+console.log(eggPrice);
+// addition 
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+// console.log(onionPrice + eggPrice);
+// subtraction
+var priceDifference = eggPrice - onionPrice;
+console.log(priceDifference);
+// multiplication 
+var eggMultiplication = eggPrice * eggQuantity;
+console.log(eggMultiplication);
+// division
+var moneyAvailable = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyAvailable / orangePrice;
+console.log(orangeQuantity);
