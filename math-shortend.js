793ce780e57a76f1a5eaf373560460bba4f0fa57
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-26 06:54:57 +0600

    math-shortend file added

---
+++ math-shortend.js
@@ -0,0 +1,23 @@
+var price1 = 30 ;
+var price2 = 10;
+var sum = price1 + price2;
+var difference = price2 - price1;
+var multiplication = price1 * price2;
+var division = price1 / price2;
+// console.log(sum);
+// console.log(difference);
+// console.log(multiplication);
+// console.log(division);
+//
+var doublePrice = price2 * 10;
+// var newPrice = price1 + 10;
+// price1 = price1 + 10;
+price1 += 10;
+console.log(price1);
+// console.log(newPrice);
+console.log(doublePrice);
+var price = 100;
+price = price - 1;
+price -= 1;
+price --;
+console.log(price);
