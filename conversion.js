50fd5ec1ec4628650d6652598455861f73228882
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-26 07:27:32 +0600

    conversion file added

---
+++ conversion.js
@@ -0,0 +1,26 @@
+var firstName = 'mofazzal';
+var lastName = 'hossain';
+var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+var firstName = 'shakil';
+var lastName = 'ahasan';
+var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+var n1 = '11';
+var n2 = 33;
+var n = n1 + n2;
+// console.log(n);
+// int Number
+var a = 5;
+
+// float number
+var b = 22.32;
+//parse integer number
+var n1 = '11';
+var n2 = 33;
+var fNumber = parseInt(n1);
+// console.log(fNumber);
+// parse float number 
+var c1 = '22.32';
+var floatNumber = parseFloat(c1);
+console.log(floatNumber);
